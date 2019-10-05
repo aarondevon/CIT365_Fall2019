51167e38ac086d77633625fb4be8c3670bcbe9c1
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-10-05 11:59:08 -0500

    start building the DeskQuote class

--- CIT365/MegaDesk-Sawyer/DeskQuote.cs
+++ CIT365/MegaDesk-Sawyer/DeskQuote.cs
@@ -7,6 +7,26 @@ using System.Threading.Tasks;
 namespace MegaDesk_Sawyer
 {
     class DeskQuote
-    {
+    { 
+        private string firstName = "";
+        private string lastName = "";
+
+        Desk desk = new Desk();
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName { get; set; }
+        public int BaseDeskPrice { get; set; }
+
+        public int DeskSurfaceArea()
+        {
+            int width = desk.Width = 5;
+            int depth = desk.Depth = 6;
+            return width * depth;
+        }
+
     }
 }
