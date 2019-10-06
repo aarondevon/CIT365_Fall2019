7c0f9d9a7d9d572517ad209ad0d74a79bb45bdc2
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-10-06 11:12:24 -0500

    Update buttons and form backgroun color

--- CIT365/MegaDesk-Sawyer/SearchQuotes.Designer.cs
+++ CIT365/MegaDesk-Sawyer/SearchQuotes.Designer.cs
@@ -34,18 +34,23 @@
             // 
             // MainMenu
             // 
-            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(156)))), ((int)(((byte)(238)))));
+            this.MainMenu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MainMenu.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenu.ForeColor = System.Drawing.Color.White;
+            this.MainMenu.Location = new System.Drawing.Point(12, 12);
             this.MainMenu.Name = "MainMenu";
-            this.MainMenu.Size = new System.Drawing.Size(75, 23);
+            this.MainMenu.Size = new System.Drawing.Size(165, 60);
             this.MainMenu.TabIndex = 0;
             this.MainMenu.Text = "&Main Menu";
-            this.MainMenu.UseVisualStyleBackColor = true;
+            this.MainMenu.UseVisualStyleBackColor = false;
             this.MainMenu.Click += new System.EventHandler(this.MainMenu_Click);
             // 
             // SearchQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(584, 361);
             this.Controls.Add(this.MainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
