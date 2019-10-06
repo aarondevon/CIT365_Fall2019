b3e96d3e1cfb2ffb9a8e9b49e7b90bb533eaa134
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-10-05 22:17:24 -0500

    Add logic for rush orders

--- CIT365/MegaDesk-Sawyer/DeskQuote.cs
+++ CIT365/MegaDesk-Sawyer/DeskQuote.cs
@@ -14,6 +14,8 @@ namespace MegaDesk_Sawyer
         public string LastName { get; set; }
         public int BaseDeskPrice { get; set; } = 200;
 
+        public string RushDays;
+
         public string getDate()
 
         {
@@ -58,6 +60,58 @@ namespace MegaDesk_Sawyer
                     return 125;
 
             }
+        }
+
+        public int PriceRush()
+        {
+            if (RushDays == "3")
+            {
+                if (DeskSurfaceArea() > 2000)
+                {
+                    return 80;
+                }
+                else if (DeskSurfaceArea() >= 1000 && DeskSurfaceArea() <= 2000)
+                {
+                    return 70;
+                }
+                else
+                {
+                    return 60;
+                }
+            }
+
+            if (RushDays == "5")
+            {
+                if (DeskSurfaceArea() > 2000)
+                {
+                    return 60;
+                }
+                else if (DeskSurfaceArea() >= 1000 && DeskSurfaceArea() <= 2000)
+                {
+                    return 50;
+                }
+                else
+                {
+                    return 40;
+                }
+            }
+
+            if (RushDays == "7")
+            {
+                if (DeskSurfaceArea() > 2000)
+                {
+                    return 40;
+                }
+                else if (DeskSurfaceArea() >= 1000 && DeskSurfaceArea() <= 2000)
+                {
+                    return 35;
+                }
+                else
+                {
+                    return 30;
+                }
+            }
+            return 0;
         }
 
         public int GetTotal()
