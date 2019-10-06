7e6a5ce117da86aa15a444a6642d0bd3ec8e9f5e
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2019-10-05 20:25:16 -0500

    Add more fields

--- CIT365/MegaDesk-Sawyer/DisplayQuote.Designer.cs
+++ CIT365/MegaDesk-Sawyer/DisplayQuote.Designer.cs
@@ -43,6 +43,10 @@
             this.labelMaterial = new System.Windows.Forms.Label();
             this.labelRushValue = new System.Windows.Forms.Label();
             this.labelRush = new System.Windows.Forms.Label();
+            this.labelCustomerValue = new System.Windows.Forms.Label();
+            this.labelCustomer = new System.Windows.Forms.Label();
+            this.labelDateValue = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Exit
@@ -75,7 +79,7 @@
             // 
             this.labelBasePrice.AutoSize = true;
             this.labelBasePrice.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelBasePrice.Location = new System.Drawing.Point(197, 22);
+            this.labelBasePrice.Location = new System.Drawing.Point(194, 97);
             this.labelBasePrice.Name = "labelBasePrice";
             this.labelBasePrice.Size = new System.Drawing.Size(90, 21);
             this.labelBasePrice.TabIndex = 6;
@@ -85,7 +89,7 @@
             // 
             this.labelBasePriceValue.AutoSize = true;
             this.labelBasePriceValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelBasePriceValue.Location = new System.Drawing.Point(345, 22);
+            this.labelBasePriceValue.Location = new System.Drawing.Point(342, 97);
             this.labelBasePriceValue.Name = "labelBasePriceValue";
             this.labelBasePriceValue.Size = new System.Drawing.Size(83, 21);
             this.labelBasePriceValue.TabIndex = 7;
@@ -95,7 +99,7 @@
             // 
             this.labelAreaPriceValue.AutoSize = true;
             this.labelAreaPriceValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAreaPriceValue.Location = new System.Drawing.Point(345, 61);
+            this.labelAreaPriceValue.Location = new System.Drawing.Point(342, 136);
             this.labelAreaPriceValue.Name = "labelAreaPriceValue";
             this.labelAreaPriceValue.Size = new System.Drawing.Size(49, 21);
             this.labelAreaPriceValue.TabIndex = 12;
@@ -105,7 +109,7 @@
             // 
             this.labelAreaPrice.AutoSize = true;
             this.labelAreaPrice.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelAreaPrice.Location = new System.Drawing.Point(197, 61);
+            this.labelAreaPrice.Location = new System.Drawing.Point(194, 136);
             this.labelAreaPrice.Name = "labelAreaPrice";
             this.labelAreaPrice.Size = new System.Drawing.Size(116, 21);
             this.labelAreaPrice.TabIndex = 11;
@@ -115,7 +119,7 @@
             // 
             this.labelTotalValue.AutoSize = true;
             this.labelTotalValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTotalValue.Location = new System.Drawing.Point(345, 211);
+            this.labelTotalValue.Location = new System.Drawing.Point(342, 286);
             this.labelTotalValue.Name = "labelTotalValue";
             this.labelTotalValue.Size = new System.Drawing.Size(49, 21);
             this.labelTotalValue.TabIndex = 14;
@@ -125,7 +129,7 @@
             // 
             this.labelTotal.AutoSize = true;
             this.labelTotal.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelTotal.Location = new System.Drawing.Point(197, 211);
+            this.labelTotal.Location = new System.Drawing.Point(194, 286);
             this.labelTotal.Name = "labelTotal";
             this.labelTotal.Size = new System.Drawing.Size(53, 21);
             this.labelTotal.TabIndex = 13;
@@ -135,7 +139,7 @@
             // 
             this.labelDrawersValue.AutoSize = true;
             this.labelDrawersValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDrawersValue.Location = new System.Drawing.Point(345, 97);
+            this.labelDrawersValue.Location = new System.Drawing.Point(342, 172);
             this.labelDrawersValue.Name = "labelDrawersValue";
             this.labelDrawersValue.Size = new System.Drawing.Size(73, 21);
             this.labelDrawersValue.TabIndex = 16;
@@ -145,7 +149,7 @@
             // 
             this.labelDrawers.AutoSize = true;
             this.labelDrawers.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDrawers.Location = new System.Drawing.Point(197, 97);
+            this.labelDrawers.Location = new System.Drawing.Point(194, 172);
             this.labelDrawers.Name = "labelDrawers";
             this.labelDrawers.Size = new System.Drawing.Size(77, 21);
             this.labelDrawers.TabIndex = 15;
@@ -155,7 +159,7 @@
             // 
             this.labelMaterialValue.AutoSize = true;
             this.labelMaterialValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelMaterialValue.Location = new System.Drawing.Point(345, 136);
+            this.labelMaterialValue.Location = new System.Drawing.Point(342, 211);
             this.labelMaterialValue.Name = "labelMaterialValue";
             this.labelMaterialValue.Size = new System.Drawing.Size(75, 21);
             this.labelMaterialValue.TabIndex = 18;
@@ -165,7 +169,7 @@
             // 
             this.labelMaterial.AutoSize = true;
             this.labelMaterial.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelMaterial.Location = new System.Drawing.Point(197, 136);
+            this.labelMaterial.Location = new System.Drawing.Point(194, 211);
             this.labelMaterial.Name = "labelMaterial";
             this.labelMaterial.Size = new System.Drawing.Size(142, 21);
             this.labelMaterial.TabIndex = 17;
@@ -175,7 +179,7 @@
             // 
             this.labelRushValue.AutoSize = true;
             this.labelRushValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelRushValue.Location = new System.Drawing.Point(348, 174);
+            this.labelRushValue.Location = new System.Drawing.Point(345, 249);
             this.labelRushValue.Name = "labelRushValue";
             this.labelRushValue.Size = new System.Drawing.Size(46, 21);
             this.labelRushValue.TabIndex = 20;
@@ -185,11 +189,51 @@
             // 
             this.labelRush.AutoSize = true;
             this.labelRush.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelRush.Location = new System.Drawing.Point(197, 174);
+            this.labelRush.Location = new System.Drawing.Point(194, 249);
             this.labelRush.Name = "labelRush";
             this.labelRush.Size = new System.Drawing.Size(50, 21);
             this.labelRush.TabIndex = 19;
             this.labelRush.Text = "Rush:";
+            // 
+            // labelCustomerValue
+            // 
+            this.labelCustomerValue.AutoSize = true;
+            this.labelCustomerValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCustomerValue.Location = new System.Drawing.Point(342, 22);
+            this.labelCustomerValue.Name = "labelCustomerValue";
+            this.labelCustomerValue.Size = new System.Drawing.Size(86, 21);
+            this.labelCustomerValue.TabIndex = 22;
+            this.labelCustomerValue.Text = "Customer";
+            // 
+            // labelCustomer
+            // 
+            this.labelCustomer.AutoSize = true;
+            this.labelCustomer.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCustomer.Location = new System.Drawing.Point(194, 22);
+            this.labelCustomer.Name = "labelCustomer";
+            this.labelCustomer.Size = new System.Drawing.Size(90, 21);
+            this.labelCustomer.TabIndex = 21;
+            this.labelCustomer.Text = "Customer:";
+            // 
+            // labelDateValue
+            // 
+            this.labelDateValue.AutoSize = true;
+            this.labelDateValue.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDateValue.Location = new System.Drawing.Point(342, 61);
+            this.labelDateValue.Name = "labelDateValue";
+            this.labelDateValue.Size = new System.Drawing.Size(50, 21);
+            this.labelDateValue.TabIndex = 24;
+            this.labelDateValue.Text = "Date";
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(194, 61);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(54, 21);
+            this.labelDate.TabIndex = 23;
+            this.labelDate.Text = "Date:";
             // 
             // DisplayQuote
             // 
@@ -197,6 +241,10 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.labelDateValue);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelCustomerValue);
+            this.Controls.Add(this.labelCustomer);
             this.Controls.Add(this.labelRushValue);
             this.Controls.Add(this.labelRush);
             this.Controls.Add(this.labelMaterialValue);
@@ -235,5 +283,9 @@
         private System.Windows.Forms.Label labelMaterial;
         private System.Windows.Forms.Label labelRushValue;
         private System.Windows.Forms.Label labelRush;
+        private System.Windows.Forms.Label labelCustomerValue;
+        private System.Windows.Forms.Label labelCustomer;
+        private System.Windows.Forms.Label labelDateValue;
+        private System.Windows.Forms.Label labelDate;
     }
 }
